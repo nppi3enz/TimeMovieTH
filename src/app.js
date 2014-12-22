0d9eafd9ee1cb212dc1df33383dfc7bb15322157
Nipitpon Chantada <nppi3enz@gmail.com>
2014-12-22 22:24:33 +0700

    version 1 update

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ var UI = require('ui');
 var ajax = require('ajax');
 var Vector2 = require('vector2');
 
-var parseFeed = function(data, quantity) {
+var parseFeed = function(data) {
   var items = [];
   //for(var i = 0; i < quantity; i++) {
   for(var i = 0; i < data.length; i++) {
@@ -16,6 +16,35 @@ var parseFeed = function(data, quantity) {
     items.push({
       title:title,
       subtitle:time
+    });
+  }
+
+  // Finally return whole array
+  return items;
+};
+var parseListMovie = function(data) {
+  var items = [];
+  //for(var i = 0; i < quantity; i++) {
+  for(var i = 0; i < data.length; i++) {
+    // Always upper case the description string
+    var name = data[i].name;
+    var rating = data[i].rating;
+    var system = data[i].system;
+    var language = data[i].language;
+    //var theatre = data[i].theatre;
+    //var showtime = [];
+    var showtime = data[i].theatre+' \n';
+    for(var j = 0; j < data[i].showtime.length; j++){
+      showtime += data[i].showtime[j]+' ';
+    }
+    var title = name;
+    var detail = '('+rating+') ('+system+') '+language;
+    
+    // Add to menu items array
+    items.push({
+      title:title,
+      subtitle:detail,
+      showtime:showtime
     });
   }
 
@@ -50,7 +79,7 @@ ajax(
   },
   function(data) {
     // Create an array of Menu items
-    var menuItems = parseFeed(data, 10);
+    var menuItems = parseFeed(data);
 
     // Construct Menu to show to user
     var resultsMenu = new UI.Menu({
@@ -63,13 +92,49 @@ ajax(
     // Add an action for SELECT
     resultsMenu.on('select', function(e) {
       console.log('Item number ' + e.itemIndex + ' was pressed!');
-      // Create the Card for detailed view
-      var detailCard = new UI.Card({
-        title:'Details',
-        subtitle:e.itemIndex+'press',
-        body: 'test'
+      
+      //fetch in cinema
+      ajax(
+      {
+        url:'http://nppi3enz.in.th/timemovieth/cinema.php?id='+e.itemIndex,
+        type:'json'
+      },
+      function(data){
+          var movieItems = parseListMovie(data);
+        
+        // Construct Menu to show to user
+          var resultsMovie = new UI.Menu({
+            sections: [{
+              title: e.item.title,
+              items: movieItems
+            }]
+          });
+        resultsMovie.on('select', function(p) {
+          var detailCard = new UI.Card({
+            title: p.item.title,
+            subtitle:p.item.subtitle,
+            body: p.item.showtime,
+            scrollable:true
+          });
+          detailCard.show();
+           /* var showT = "";
+            for(var x=0;x<p.showtime.length;x++){
+              showT += p.showtime[x];
+            }
+            // Create the Card for detailed view
+            var detailCard = new UI.Card({
+              title:p.name,
+              subtitle:p.detail,
+              body: p.cinema+'\n'+showT
+            });
+            detailCard.show();*/
+        });
+        
+        resultsMovie.show();
+        //resultsMenu.hide();
       });
-      detailCard.show();
+      
+      
     });
 
     // Show the Menu, hide the splash
